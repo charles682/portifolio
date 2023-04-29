be40e0e791459515a452ec9a17f2e381f3ff4101
Charles Melo <77029992+charles682@users.noreply.github.com>
2023-04-29 10:43:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,111 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+});
+
+
+$('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+});
+
+
+class FormSubmit {
+    constructor(settings) {
+      this.settings = settings;
+      this.form = document.querySelector(settings.form);
+      this.formButton = document.querySelector(settings.button);
+      if (this.form) {
+        this.url = this.form.getAttribute("action");
+      }
+      this.sendForm = this.sendForm.bind(this);
+    }
+  
+    displaySuccess() {
+    //  this.form.innerHTML = this.settings.success;
+
+     this.form = alert("Enviado com sucesso");
+ 
+    }
+  
+    displayError() {
+    //   this.form.innerHTML = this.settings.error;
+        this.form = alert("Error ao enviar formulario")
+    }
+  
+    getFormObject() {
+      const formObject = {};
+      const fields = this.form.querySelectorAll("[name]");
+      fields.forEach((field) => {
+        formObject[field.getAttribute("name")] = field.value;
+      });
+      return formObject;
+    }
+  
+    onSubmission(event) {
+      event.preventDefault();
+      event.target.disabled = true;
+      event.target.innerText = "Enviando...";
+    }
+  
+    async sendForm(event) {
+      try {
+        this.onSubmission(event);
+        await fetch(this.url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(this.getFormObject()),
+        });
+        this.displaySuccess();
+      } catch (error) {
+        this.displayError();
+        throw new Error(error);
+      }
+    }
+  
+    init() {
+      if (this.form) this.formButton.addEventListener("click", this.sendForm);
+      return this;
+    }
+  }
+  
+  const formSubmit = new FormSubmit({
+  
+    form: "[data-form]",
+    button: "[data-button]",
+    // success: "<h1 class='success'>Mensagem enviada!</h1>",
+    // error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
+  });
+  formSubmit.init();
+
+  
